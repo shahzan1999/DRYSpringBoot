bd3b37976bff953d860f2671cd5cc2e46e5cfa4a
Mohd Shahzan <shahzan1999@gmail.com>
2021-06-12 12:41:22 +0530

    -initial commit

---
+++ dry-springboot/src/main/java/demo/DrySpringbootApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrySpringbootApplication.class, args);
+	}
+
+}
